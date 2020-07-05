785f7874e9cf4072c6715e5f9b4ae860cdd655a5
Brandon Gottlob <info@bgottlob.com>
2020-07-04 22:19:08 -0400

    WIP: registration tests

--- tests/test_appsec.py
+++ tests/test_appsec.py
@@ -15,9 +15,30 @@ def app():
 def client(app):
     return app.test_client()
 
+# Helpers
+def register(client, username, password, mfa):
+    return client.post('/register', data = {
+        'uname': username,
+        'pword': password,
+        '2fa': mfa
+    }, follow_redirects = True)
+
+def register_message(success, username):
+    return 
+
 # Tests that a client initially does not have a session token
 def test_no_token(client):
     with client:
         client.get('/')
         assert (not 'username' in session)
         assert (len(session) == 0)
+
+
+def test_register_and_login(client):
+    rv = register(client, 'testusername', 'testpassword', '6091234567').data.decode('utf-8')
+    assert 'Success' in rv
+
+
+#def test_incorrect_login():
+#
+#def test_incorrect_mfa():
