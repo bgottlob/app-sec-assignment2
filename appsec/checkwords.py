d7e9c3f42ebbe49264058f5f4dd47021f57d0a37
Brandon Gottlob <info@bgottlob.com>
2020-07-07 21:14:35 -0400

    Delete user input files before checkWords response

--- appsec/checkwords.py
+++ appsec/checkwords.py
@@ -1,6 +1,8 @@
 import os
 import pathlib
+import shutil
 import subprocess
+import uuid
 
 from flask import (
     Blueprint, redirect, render_template, request, session, url_for
@@ -8,24 +10,36 @@ from flask import (
 
 bp = Blueprint('checkwords', __name__)
 
+currdir = os.path.dirname(__file__)
+inputdir = os.path.join(currdir, 'userinput')
+
+# Clear the contents of the user input directory if it exists, then recreate it
+# to hold user input files
+if os.path.exists(inputdir):
+    shutil.rmtree(inputdir)
+pathlib.Path(inputdir).mkdir(exist_ok=True)
+
 @bp.route('/checkWords', methods = ['GET', 'POST'])
 def checkWords():
     if request.method == 'GET':
         return redirect(url_for('index'))
     if 'username' in session:
         input_text = request.values['inputtext']
-        inputdir = os.path.join(os.path.dirname(__file__), 'userinput')
-        pathlib.Path(inputdir).mkdir(exist_ok=True)
-        wordlist = os.path.join(os.path.dirname(__file__), 'spell', 'wordlist.txt')
-        filename = os.path.join(inputdir, session['username'] + '.txt')
-        exe = os.path.join(os.path.dirname(__file__), 'spell', 'a.out')
 
-        with open(filename, 'w') as f:
+        wordlist = os.path.join(currdir, 'spell', 'wordlist.txt')
+        filepath = os.path.join(inputdir, f'{uuid.uuid4()}.txt')
+        exe = os.path.join(currdir, 'spell', 'a.out')
+
+        with open(filepath, 'w') as f:
             f.write(request.values['inputtext'])
 
-        cmd = exe + ' ' + filename + ' ' + wordlist
-
+        cmd = f'{exe} {filepath} {wordlist}'
         res = subprocess.check_output([cmd], shell=True).decode('utf-8')
+
+        # Delete user input file after misspelled words are found, it is no longer needed
+        #os.remove(filepath)
         
         return render_template('index.html', textout = input_text, misspelled = res.rstrip().replace('\n', ', '))
+
+    # Redirect users who are not logged in to the home page
     return redirect(url_for('index'))
